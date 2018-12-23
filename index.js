fdb15db6ab334d5a9f7919f2c53b23c3b0034673
Michael <chaphlux@yahoo.com>
2018-12-23 18:50:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ console.log (string.toLowerCase())
 }
 
 function sayHiToGrandma(string){
-  var lowercase = "string"
-  if(lowercase === lowercase.toLowerCase){return 'I can't hear you!}
+  var lowercase = string;
+  if(lowercase === lowercase.toLowerCase())
+  {return "I can't hear you!"}
 }
